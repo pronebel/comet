9e091be0192bb395614c0a66023680728c87e3c9
Ali Chamas <ali.chamas@play.co>
2022-10-29 21:21:46 +1100

    basic breakdowns for new transform gizmos

--- src/editor/main.ts
+++ src/editor/main.ts
@@ -6,7 +6,7 @@ import './style/theme.css';
 import './style/dark-theme.css';
 import './style/light-theme.css';
 // test
-import './transformTest2';
+import './transformTest3';
 // import App from './views/App.svelte';
 
 // const target = document.getElementById('app') as HTMLElement;
