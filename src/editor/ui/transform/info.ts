e6132a728f20e8e6c6b214375437e02cbbb0b096
Ali Chamas <ali.chamas@play.co>
2022-11-02 17:30:40 +1100

    add rotation to gizmo info

--- src/editor/ui/transform/info.ts
+++ src/editor/ui/transform/info.ts
@@ -1,8 +1,12 @@
-import { Container } from 'pixi.js';
+import { Graphics } from 'pixi.js';
 
+import { degToRad } from '../../../core/util/geom';
 import type { TransformGizmoFrame } from './frame';
+import { RotateOperation } from './operations/rotate';
 
-export class TransformGizmoInfo extends Container
+const rotationRadius = 30;
+
+export class TransformGizmoInfo extends Graphics
 {
     constructor(public readonly frame: TransformGizmoFrame)
     {
@@ -13,6 +17,28 @@ export class TransformGizmoInfo extends Container
 
     public update()
     {
-        // const { frame: { gizmo } } = this;
+        const { frame: { gizmo: { operation } } } = this;
+
+        this.clear();
+
+        if (operation instanceof RotateOperation)
+        {
+            this.drawRotation();
+        }
+    }
+
+    public drawRotation()
+    {
+        const { frame: { gizmo: { pivotGlobalPos, rotation } } } = this;
+        const p0 = pivotGlobalPos;
+        const p1 = { x: p0.x + rotationRadius, y: p0.y };
+
+        this.lineStyle(1, 0xffff00, 1);
+        this.beginFill(0xffff00, 0.2);
+        this.moveTo(p0.x, p0.y);
+        this.lineTo(p1.x, p1.y);
+        this.arc(p0.x, p0.y, rotationRadius, 0, degToRad(rotation));
+        this.lineTo(p0.x, p0.y);
+        this.endFill();
     }
 }
